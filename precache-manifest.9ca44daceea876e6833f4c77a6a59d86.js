c96e8c1184ecaef1cd8159b9e284a78dd69ef87a
kaviza90 <vieralaqueviera@gmail.com>
2019-10-03 00:26:04 -0500

    Updates

---
+++ precache-manifest.9ca44daceea876e6833f4c77a6a59d86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5498da5558292b9bc5573bf43b3aaa6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "05462520e20fcbb58d19",
+    "url": "/Portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "310b64553ffca4f3827f",
+    "url": "/Portfolio/static/js/2.c8972d9e.chunk.js"
+  },
+  {
+    "revision": "05462520e20fcbb58d19",
+    "url": "/Portfolio/static/js/main.1eff6396.chunk.js"
+  },
+  {
+    "revision": "6e290e45fa9068bd6272",
+    "url": "/Portfolio/static/js/runtime-main.faed0cb7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
